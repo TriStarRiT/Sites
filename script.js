21b4cbc4c23e4b3aa1fb15e00280cc2e42ce4ba5
GRiT <alex-kashin-03@mail.ru>
2022-08-31 21:31:58 +0300

    added some scripts

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function rotate(){
-  document.getElementById(cal).innerHTML="<strong>Black</strong>+White"
+var i;
+function recolor(i){
+  document.getElementById(i).style.color="black"
+}
+function uncolor(i){
+  document.getElementById(i).style.color="#4f4f4f"
+}
+function spin(){
+  document.getElementById('call').innerHTML="<strong>Black</strong>&timesWhite"
+}
+function unspin(){
+  document.getElementById('call').innerHTML="<strong>Black</strong>+White"
 }
